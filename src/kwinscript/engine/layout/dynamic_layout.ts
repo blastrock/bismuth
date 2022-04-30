9d7f10bf8a3eee786ab2c33502b468fb0c14e09e
Philippe Daouadi <philippe@ud2.org>
2022-04-29 20:41:14 -0400

    debug and stuff

--- src/kwinscript/engine/layout/dynamic_layout.ts
+++ src/kwinscript/engine/layout/dynamic_layout.ts
@@ -34,28 +34,61 @@ export class DynamicLayoutPart {
     this.subParts = [];
   }
 
+  // public adjust(
+  //   area: Rect,
+  //   tiles: EngineWindow[],
+  //   basis: EngineWindow,
+  //   delta: RectDelta
+  // ): RectDelta {
+  //   const weights = LayoutUtils.adjustAreaWeights(
+  //     area,
+  //     tiles.map((tile) => tile.weight),
+  //     this.config.tileLayoutGap,
+  //     tiles.indexOf(basis),
+  //     delta,
+  //     false
+  //   );
+
+  //   weights.forEach((weight, i) => {
+  //     tiles[i].weight = weight * tiles.length;
+  //   });
+
+  //   const idx = tiles.indexOf(basis);
+  //   return new RectDelta(
+  //     delta.east,
+  //     delta.west,
+  //     idx === tiles.length - 1 ? delta.south : 0,
+  //     idx === 0 ? delta.north : 0
+  //   );
+  // }
+
   public split(direction: SplitDirection, currentWindow: EngineWindow): number {
     const replacePart = (position: number) => {
       const newPart = new DynamicLayoutPart(this.config, direction);
       newPart.subParts.push(currentWindow.id);
       this.subParts[position] = newPart;
+      console.log('done');
     };
 
     for (let i = 0; i < this.subParts.length; ++i) {
       const subPart = this.subParts[i];
       if (subPart === currentWindow.id) {
+        console.log('found current window', i);
         // If this part contains a single window, replace it instead of nesting
         // a new split. This operation will be handled by the caller.
         if (this.subParts.length === 1) return 2;
 
+        console.log('creating new part');
         replacePart(i);
         return 1;
       } else if (subPart instanceof DynamicLayoutPart) {
         const splitResult = subPart.split(direction, currentWindow);
         if (splitResult === 1) {
+          console.log('sub part did the split');
           return 1;
         } else if (splitResult === 2) {
           // The subpart can't handle the split itself, replace it completely.
+          console.log('replacing child part');
           replacePart(i);
         }
       }
@@ -68,14 +101,18 @@ export class DynamicLayoutPart {
     // Make sure that all windows are referenced. Remove the ones that have
     // disappeared, and add the potentially new ones at the end. Note that most
     // window additions should be handled by newWindow().
+    console.log('part prepare');
     for (let i = 0; i < this.subParts.length; ++i) {
+      console.log('part iteration');
       const subPart = this.subParts[i];
       if (subPart instanceof DynamicLayoutPart) {
         subPart.prepare(tiles, false);
       } else {
         if (tiles.length > 0 && subPart === tiles[0].id) {
+          console.log('part matched window id', subPart);
           tiles.shift();
         } else {
+          console.log('part did not match, removing', subPart);
           this.subParts.splice(i, 1);
           --i;
         }
@@ -84,34 +121,41 @@ export class DynamicLayoutPart {
     // Remaining windows
     if (topLevel) {
       while (tiles.length > 0) {
+        console.log('adding remaining window', tiles[0].id);
         this.subParts.push(tiles[0].id);
         tiles.shift();
       }
     }
+    console.log('part prepared');
   }
 
   public handelNewWindow(
     currentWindow: EngineWindow,
     newWindow: EngineWindow
   ): boolean {
+    console.log(`adding new window ${newWindow.id}`);
     // The new window was inserted in the WindowStore just after the current
     // window, so find it so that we can insert it at the same place, and in the
     // same subpart.
     for (let i = 0; i < this.subParts.length; ++i) {
       const subPart = this.subParts[i];
       if (subPart instanceof DynamicLayoutPart) {
+        console.log('recursing');
         if (subPart.handelNewWindow(currentWindow, newWindow)) return true;
       } else {
         if (subPart === currentWindow.id) {
+          console.log('found the current window, adding the new one after');
           this.subParts.splice(i + 1, 0, newWindow.id);
           return true;
         }
       }
     }
+    console.log('where is the current window??');
     return false;
   }
 
   public apply(area: Rect, tiles: EngineWindow[]): Rect[] {
+    console.log('part apply');
     const partAreas = LayoutUtils.splitAreaWeighted(
       area,
       this.subParts.map((_) => 1.0),
@@ -121,6 +165,7 @@ export class DynamicLayoutPart {
     const rects: Array<Rect> = [];
     this.subParts.forEach((subPart, i) => {
       if (subPart instanceof DynamicLayoutPart) {
+        console.log('applying subpart');
         const subRects = subPart.apply(partAreas[i], tiles);
         rects.splice(rects.length, 0, ...subRects);
       } else {
@@ -130,10 +175,12 @@ export class DynamicLayoutPart {
             `apply: unexpected window id: ${tiles[0].id}, expected: ${subPart}`
           );
         }
+        console.log('applying window', subPart);
         rects.push(partAreas[i]);
         tiles.shift();
       }
     });
+    console.log('part applied');
     return rects;
   }
 
@@ -144,6 +191,19 @@ export class DynamicLayoutPart {
       else console.log("  ".repeat(depth), subPart);
     });
   }
+
+  // public hasWindow(window: EngineWindow): boolean {
+  //   for (const subPart of this.subParts) {
+  //     if (typeof subPart === 'string') {
+  //       if (subPart === window.id)
+  //         return true;
+  //     }
+  //     else {
+  //       return subPart.hasWindow(window);
+  //     }
+  //   }
+  //   return false;
+  // }
 }
 
 export default class DynamicLayout implements WindowsLayout {
@@ -161,9 +221,11 @@ export default class DynamicLayout implements WindowsLayout {
   constructor(config: Config) {
     this.config = config;
 
+    console.log('new part');
     this.parts = new DynamicLayoutPart(this.config);
 
     this.parts.gap = this.config.tileLayoutGap;
+    console.log('constructed');
   }
 
   public handelNewWindow(
@@ -173,31 +235,59 @@ export default class DynamicLayout implements WindowsLayout {
     this.parts.handelNewWindow(currentWindow, newWindow);
   }
 
+  // public adjust(
+  //   area: Rect,
+  //   tiles: EngineWindow[],
+  //   basis: EngineWindow,
+  //   delta: RectDelta
+  // ): void {
+  //   this.parts.adjust(area, tiles, basis, delta);
+  // }
+
   public apply(
     _controller: Controller,
     tileables: EngineWindow[],
     area: Rect
   ): void {
-    tileables.forEach((tileable) => (tileable.state = WindowState.Tiled));
-    this.parts.dump();
+    try {
+      console.log('layout apply', tileables.map(x => x.id));
+      tileables.forEach((tileable) => (tileable.state = WindowState.Tiled));
+      console.log('parts');
+      this.parts.dump();
 
-    // Applying the layout must be done in two steps. First we prepare to detect
-    // all windows we might have missed (like when the layout is first used) and
-    // add them to the layout. Then we can apply the layout, once it has been
-    // synchronized with the current windows.
-    this.parts.prepare(tileables.slice());
-    const rects = this.parts.apply(area, tileables.slice());
-    rects.forEach((geometry, i) => {
-      tileables[i].geometry = geometry;
-    });
+      // Applying the layout must be done in two steps. First we prepare to detect
+      // all windows we might have missed (like when the layout is first used) and
+      // add them to the layout. Then we can apply the layout, once it has been
+      // synchronized with the current windows.
+      console.log('layout prepare');
+      this.parts.prepare(tileables.slice());
+      const rects = this.parts.apply(area, tileables.slice());
+      console.log('layout result:', rects);
+      rects.forEach((geometry, i) => {
+        tileables[i].geometry = geometry;
+      });
+      console.log('layout applied');
+    } catch (e) {
+      console.log(e, (e as Error).stack);
+      throw e;
+    }
   }
+
+  // public clone(): WindowsLayout {
+  //   const other = new DynamicLayout(this.config);
+  //   other.masterRatio = this.masterRatio;
+  //   other.numMaster = this.numMaster;
+  //   return other;
+  // }
 
   public executeAction(engine: Engine, action: Action): void {
     this.parts.dump();
     if (action instanceof SplitPartHorizontally) {
+      console.log('split horizontal triggered');
       const currentWindow = engine.currentWindow();
       if (currentWindow) this.parts.split("horizontal", currentWindow);
     } else if (action instanceof SplitPartVertically) {
+      console.log('splitting vertically');
       const currentWindow = engine.currentWindow();
       if (currentWindow) this.parts.split("vertical", currentWindow);
     } else {
